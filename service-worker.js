9e5a6960cc64b87f58c51a148be7d58b9044aa4b
YudelKeyla <yudelnc@gmail.com>
2026-05-24 00:45:22 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE = 'academico-v1';
+const urls = ['./','index.html','style.css','script.js','manifest.json'];
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(urls)));
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
