c5c8447ce29720a84813cab92134e61aee1b7bc2
YudelKeyla <yudelnc@gmail.com>
2026-05-26 10:03:39 +0530

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,33 @@
-const CACHE = 'academico-v1';
-const urls = ['./','index.html','style.css','script.js','manifest.json'];
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(urls)));
+const CACHE_NAME = 'unicontrol-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
+  );
 });
